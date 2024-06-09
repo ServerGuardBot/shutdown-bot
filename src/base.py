adf9516c830141d0e397667ae29a9ebe01aaf409
Reapimus <29674188+Reapimus@users.noreply.github.com>
2024-06-10 01:13:56 +1000

    Add Campground embed

--- src/base.py
+++ src/base.py
@@ -26,9 +26,17 @@ async def send_shutdown(ctx):
         title="this post",
         link="https://www.guilded.gg/server-guard/groups/D57rgP7z/channels/bd1c327a-44e4-4c63-8eda-3c9a5b417c19/announcements/16nGrG1y"
     )
+    cg_link = guilded.utils.link(
+        title="here",
+        link="https://discord.gg/Savkc9RvPw"
+    )
     await ctx.reply(embed=EMBED_STANDARD(
         title="Server Guard has shutdown.",
         description=f"Server Guard will not be responding to any commands. Please refer to {link} for more information.\n\nIt has been fun maintaining this bot, goodbye!\n\n- Reapimus"
+    ))
+    await (isinstance(ctx, guilded.Message) and ctx.channel.send or ctx.send)(embed=EMBED_STANDARD(
+        title="Alternative chatting app",
+        description=f"I am also now working on an alternative chatting app with some other developers! anyone interested can find an invite to its Discord server {cg_link}!"
     ))
 
 @bot.event
